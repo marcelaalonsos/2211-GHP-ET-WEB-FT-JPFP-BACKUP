73e47ac34ab63ce533d4bfab25de7c5a35e7ff9e
marcela <marcela.alonso.s@gmail.com>
2022-12-14 17:29:55 -0500

    changes to backup

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 const db = new Sequelize({
   host: 'localhost',
   port: 5432,
-  database: 'acme_schools_db',
+  database: 'acme_schools_db2',
   dialect: 'postgres',
   username: 'postgres',
   password: 'admin',
